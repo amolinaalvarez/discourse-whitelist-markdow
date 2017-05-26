fd7e2e911f38d6a8ff09d333853d01e8d0af110f
Alvaro Molina Alvarez <alvaro@exo.works>
2017-05-26 09:42:41 +0200

    adding plugin.rb and javascript code

---
+++ assets/javascript/whitelist-markdown.js
@@ -0,0 +1,3 @@
+(function () {
+  Discourse.Markdown.whiteListTag('div', 'class', 'header');
+})();
---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-whitelist-markdow
+# about: Basic template to allow different html elements and attributes when creating a class.
+# version: 0.0.1
+# authors: Alvaro Molina Alvarez
+# url: https://github.com/amolinaalvarez/discourse-whitelist-markdow
+
+register_asset "javascript/whitelist-markdown.js.es6", :server_side
